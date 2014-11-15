b068e77d4315ec0e7e791372577b3fcbd640aec1
Aaron Williamson <wikea1@gmail.com>
2014-11-14 22:33:13 -0800

    Created app

--- todolist/FirstViewController.swift
+++ todolist/FirstViewController.swift
@@ -8,18 +8,56 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var toDoItems:[String] = []
 
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet var tasksTable:UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoItems.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel.text = toDoItems[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView!, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath!) {
+        if (editingStyle == UITableViewCellEditingStyle.Delete) {
+            toDoItems.removeAtIndex(indexPath.row)
+            
+            let immutableToDoItems = toDoItems
+            NSUserDefaults.standardUserDefaults().setObject(immutableToDoItems, forKey: "toDoItems")
+            NSUserDefaults.standardUserDefaults().synchronize()
+            
+            tasksTable.reloadData()
+        }
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        if var storedToDoItems : AnyObject = NSUserDefaults.standardUserDefaults().objectForKey("toDoItems") {
+        
+            toDoItems = []
+            
+            for var i = 0; i < storedToDoItems.count; ++i {
+                toDoItems.append(storedToDoItems[i] as NSString)
+            }
+        
+        }
+        
+        tasksTable.reloadData()
+    }
+    
 }
 
--- todolist/SecondViewController.swift
+++ todolist/SecondViewController.swift
@@ -8,18 +8,39 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var toDoItem: UITextField!
+    
+    
+    @IBAction func addItem(sender: AnyObject) {
+    
+        toDoItems.append(toDoItem.text)
+        let immutableToDoItems = toDoItems
+        NSUserDefaults.standardUserDefaults().setObject(immutableToDoItems, forKey: "toDoItems")
+        NSUserDefaults.standardUserDefaults().synchronize()
+        
+        toDoItem.text = ""
+        
+        self.view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+    }
 
 }
 
